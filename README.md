de155525fad0a3525f7177b2e2a60cdb0d0fc3e6
wardymate <chriswarddev@gmail.com>
2015-03-06 12:24:38 +0000

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # round-one-mar-6
+
+A very Simple exercise to get us started
+
+1. Write a JavaScript function that reverse a number.
+
+Example x = 123456789;
+Expected Output : 987654321
+
+
+Use Jasmine to help write it.
+
--- src/reverser.js
+++
@@ -1,9 +0,0 @@
-var Reverser = function() {
-  this.output = 1;
-};
-
-Reverser.prototype.makeReverse = function(input_number) {
-  var n = input_number.toString();
-  var n = n.split('').reverse().join('');
-  this.output = parseInt(n);
-};
